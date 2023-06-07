c9efc26014bad467b07c1769815680d71e32b7db
terry <digihero@gmail.com>
2023-06-07 11:22:05 +0800

    多平台构建

--- php/php8-nginx-alpine/build.sh
+++ php/php8-nginx-alpine/build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker build -t wkii/php-docker:nginx-fpm8-alpine -f Dockerfile .
+# for docker build
+# docker build -t wkii/php-docker:nginx-fpm8-alpine -f Dockerfile .
+
+# for docker buildx
+# before use: docker buildx create --name mybuilder --driver docker-container && docker buildx use mybuilder
+docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -t wkii/php-docker:nginx-fpm8-alpine -f Dockerfile . --push
